d4b554342f1e981fb37d6761bc8198015c6bac4c
alisherova <alisherovanurjahon@gmail.com>
2024-06-20 13:46:27 +0200

    add more models

--- models/Book.js
+++ models/Book.js
@@ -5,6 +5,10 @@ const BookSchema = new mongoose.Schema({
   author: { type: String, trim: true, required: true },
   description: { type: String, trim: true, required: false },
   price: { type: Number, required: true, min: 0 },
+  img: { type: String, trim: true, required: false },
+  category: { type: String, trim: true, required: false },
+  pdflink: { type: String, trim: true, required: false },
+  infolink: { type: String, trim: true, required: false },
 });
 
 const BookModel = mongoose.model("books", BookSchema);
